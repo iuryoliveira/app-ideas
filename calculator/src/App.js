23f54d05a09ad850ebd856174bce7eb92315be61
Iury <iury@rarolabs.com.br>
2020-03-15 22:14:24 -0300

    componente básico da calculadora

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Calculator from './component/calculator'
 
 function App() {
   return (
     <div className="App">
-      <h1>Calculadora</h1>
+      <Calculator />
     </div>
   );
 }
