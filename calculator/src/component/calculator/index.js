eefe8faa08c3976bf8b2335b8befecf83951c228
Iury <iury@rarolabs.com.br>
2020-03-16 22:26:05 -0300

    design básico calculadora

--- calculator/src/component/calculator/index.js
+++ calculator/src/component/calculator/index.js
@@ -10,23 +10,26 @@ export default class Calculator extends Component {
                 </div>
                 <div className="teclado">
                     <div className="teclado-numerico">
-                        <div className="horizontal">
-                            <button>1</button>
-                            <button>2</button>
-                            <button>3</button>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+
+                        <div className="last-item">
+                            <button className="last-button">0</button>
                         </div>
-                        <div className="horizontal">
-                            <button>4</button>
-                            <button>5</button>
-                            <button>6</button>
-                        </div>
-                        <div className="horizontal">
-                            <button>7</button>
-                            <button>8</button>
-                            <button>9</button>
-                        </div>
+                    </div> 
+                    <div className="teclado-operacao">
+                        <button>+</button>
+                        <button>-</button>
+                        <button>*</button>
+                        <button>/</button>
                     </div>
-                    <div className="teclado-operacao"></div>
                 </div>
                 
             </div>
