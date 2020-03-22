f9e786ec157a88606e984e3f34510fc63cdb00a3
Iury <iury@rarolabs.com.br>
2020-03-21 23:52:01 -0300

    loading selected person name

--- cause-effect/src/App.js
+++ cause-effect/src/App.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import Header from './components/header';
+import React, { Component } from 'react';
+import Article from './components/article';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+import { HeaderList } from './styles';
+
+export default class App extends Component {
+  state = {
+    people: [
+      {
+        name: 'Iury Oliveira',
+        street: 'Euclides da Cunha',
+        country: 'Brazil',
+        telephone: '55-(31)99999-5555',
+        birthDay: '08/05/1997'
+      },
+      {
+        name: 'Phil Collins',
+        street: 'Georgy Street',
+        country: 'USA',
+        telephone: '5599999-5555',
+        birthDay: '24/05/1997'
+      },
+      {
+        name: 'George Silva',
+        street: 'Paul Boutcosta',
+        country: 'Brazil',
+        telephone: '55-(31)95555-9999',
+        birthDay: '09/05/1995'
+      }
+    ],
+
+    selectedPerson: 0
+  };
+
+  handleSelection = async (data, position) => {
+    console.log(`loading data for ${data.name}`);
+    await this.setState({ selectedPerson: position })
+  }
+
+  render() {
+    const { people, selectedPerson } = this.state;
+
+    return (
+      <div className="App">
+        <center>
+          <h1>ReactJS</h1>
+          <HeaderList>
+            {people.map((data, i) => {
+              return (<li key={i} onClick={() => this.handleSelection(data, i)}><a href='./#'>{data.name}</a></li>)
+            })}
+          </HeaderList>
+        </center>
+
+        <Article selectedPerson={people[selectedPerson]}/>
+      </div>
+    );
+  }
 }
-
-export default App;
